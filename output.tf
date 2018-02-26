69eb451e6f2e54ac3c966e33c5956a98c250ae49
Fraser Pollock <fraserpol@gmail.com>
2018-02-25 16:51:33 -0800

    Fixing output

--- output.tf
+++ output.tf
@@ -7,8 +7,3 @@ output "demo_subnet_id" {
   value = "${aws_subnet.demo_subnet.id}"
   description = "This is the Subnet ID for later use"
 }
-
-output "instance_nic_consumable" {
-  value = "${aws_network_interface.demo_nic.id}"
-  description = "The Network Interface for later use"
-}
